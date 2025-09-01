48c039d8e5d416dd0f7d014720d49ccad96f5e0c
base-scrabble <basedscrabbleteam@gmail.com>
2025-09-01 02:14:56 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,14 @@
+import { Link } from "react-router-dom";
 
+export default function Navbar() {
+  return (
+    <nav className="bg-white shadow p-4 flex justify-between">
+      <h1 className="font-bold text-xl text-indigo-600">Based Scrabble</h1>
+      <div className="space-x-4">
+        <Link to="/" className="hover:text-indigo-600">Home</Link>
+        <Link to="/game" className="hover:text-indigo-600">Play</Link>
+        <Link to="/waitlist" className="hover:text-indigo-600">Waitlist</Link>
+      </div>
+    </nav>
+  );
+}
