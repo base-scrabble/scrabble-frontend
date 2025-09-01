87d963a67f06ef24906f9ca83d496e52bdddcaad
base-scrabble <basedscrabbleteam@gmail.com>
2025-09-01 02:55:41 +0100

    Update and rename LeaderboardPage.jsx to Leaderboard.jsx

---
+++ src/pages/Leaderboard.jsx
@@ -0,0 +1,8 @@
+export default function Leaderboard() {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold mb-4">Leaderboard</h1>
+      <p className="text-gray-600">Top players will be displayed here.</p>
+    </div>
+  );
+}
