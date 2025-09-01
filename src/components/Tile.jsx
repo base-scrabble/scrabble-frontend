a77b4daa271af352182577ba931452ab5d9ef253
base-scrabble <basedscrabbleteam@gmail.com>
2025-09-01 02:30:32 +0100

    Update Tile.jsx

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,1 +1,7 @@
-
+export default function Tile() {
+  return (
+    <div className="w-8 h-8 bg-white border flex items-center justify-center text-sm">
+      {/* Letter goes here */}
+    </div>
+  );
+}
